8ddf2f6f7f0cd4a6f5585c2f641c0f8158aa88b2
admin <admin@DESKTOP-1LQRC0G>
2018-03-30 12:46:37 +0800

    分支特有2

--- LearnVSGit/LearnVSGit/Program.cs
+++ LearnVSGit/LearnVSGit/Program.cs
@@ -33,5 +33,12 @@ namespace LearnVSGit
         /// <param name="a"></param>
         /// <param name="b"></param>
         private void max(int a, int b) { }
+
+        /// <summary>
+        /// 分支特有2
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        private void avg(int a, int b) { }
     }
 }
