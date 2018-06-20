0a3885959a3ed4f7a65f2799a25f45544ab5adbb
admin <admin@DESKTOP-1LQRC0G>
2018-06-20 19:36:57 +0800

    commit one

--- LearnVSGit/LearnVSGit/Program.cs
+++ LearnVSGit/LearnVSGit/Program.cs
@@ -34,14 +34,5 @@ namespace LearnVSGit
         /// <param name="a"></param>
         /// <param name="b"></param>
         private void max(int a, int b) { }
-
-        /// <summary>
-        /// 分支特有2
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        private void avg(int a, int b) { }
-        private void sum(int a, int b) { }
-        private void sumSC123(int a, int b) { }
     }
 }
